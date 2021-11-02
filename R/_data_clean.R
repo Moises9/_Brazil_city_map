e9933410f8e5bba38152e66522ba0f6544ec5a9c
Moises9 <moises.estat@gmail.com>
2021-11-02 00:19:58 -0300

    # test function

--- R/_data_clean.R
+++ R/_data_clean.R
@@ -1,4 +1,11 @@
 
 
-# library souces =====
+# https://concla.ibge.gov.br/classificacoes/por-tema/codigo-de-areas/codigo-de-areas
+# https://www.rdocumentation.org/packages/geobr/versions/1.0
 
+# library sources =====
+library(esquisse)
+library(geobr)
+library(leaflet)
+
+func_data = list_geobr()
